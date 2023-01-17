bf83b90f8a889fc49ac380a790ae463143d90c1e
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-17 17:23:15 +0100

    Add Clang format check for Node.js source code inside VCS

--- server/server_app.js
+++ server/server_app.js
@@ -2,10 +2,9 @@
 
 const express = require('express');
 const mysql = require("mysql");
-require('dotenv').config({ path: 'database.env' });
-const { PrismaClient } = require('@prisma/client');
+require('dotenv').config({path : 'database.env'});
+const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
-
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -13,26 +12,25 @@ const HOST = '0.0.0.0';
 const app = express();
 
 const connection = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+  connectionLimit : 10,
+  host : process.env.host,
+  user : process.env.user,
+  password : process.env.password,
+  database : process.env.database,
 });
 
 /**
  * A basic function to demonstrate the Jsdoc functionnality !
- * @param {*} title 
- * @param {*} author 
+ * @param {*} title
+ * @param {*} author
  */
-function doc_example(title, author) {
+function doc_example(title, author) {}
 
-}
-
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   // Allow access request from any computers
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers",
+             "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE,PATCH');
   res.header('Access-Control-Allow-Credentials', true);
   if ('OPTIONS' == req.method) {
@@ -42,21 +40,18 @@ app.use(function (req, res, next) {
   }
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get('/', (req, res) => { res.send('Hello World'); });
 
-// about.json route answering the client IP, the current time and the services data
+// about.json route answering the client IP, the current time and the services
+// data
 app.get('/about.json', (req, res) => {
   try {
     const about = {};
-    about.client = { host: req.ip }
-    about.server = {
-      current_time: Date.now(),
-      services: []
-    }
-    // TODO fetch the services data from DB with the IP of the client
-    res.json(about);
+    about.client = {host : req.ip} about.server = {
+      current_time : Date.now(),
+      services : []
+    } // TODO fetch the services data from DB with the IP of the client
+                                                  res.json(about);
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
@@ -68,12 +63,12 @@ app.get('/database', (req, res) => {
   connection.query("SELECT * FROM Student", (err, rows) => {
     if (err) {
       res.json({
-        success: false,
+        success : false,
         err,
       });
     } else {
       res.json({
-        success: true,
+        success : true,
         rows,
       });
     }
@@ -84,9 +79,9 @@ app.get('/database', (req, res) => {
 app.get('/database/get/user', async (req, res) => {
   try {
     const allUsers = await prisma.user.findMany({
-      include: {
-        posts: true,
-        profile: true,
+      include : {
+        posts : true,
+        profile : true,
       },
     }); /// GET all the user and her relations
     res.send(allUsers);
@@ -98,7 +93,8 @@ app.get('/database/get/user', async (req, res) => {
 
 app.get('/database/get/try', async (req, res) => {
   try {
-    const allUsers = await prisma.try.findMany(); /// GET all the user and her relations
+    const allUsers =
+        await prisma.try.findMany(); /// GET all the user and her relations
     res.send(allUsers);
   } catch (err) {
     console.log(err);
@@ -107,39 +103,40 @@ app.get('/database/get/try', async (req, res) => {
 });
 
 /// Create a data with prisma example
-app.get('/database/post/user', async (req, res) => { /// ?name=alice&posts=hey&profile=bio
-  try {
-    await prisma.user.create({
-      data: {
-        name: req.query.name,
-        email: req.query.name + '@prisma.io',
-        posts: {
-          create: { title: req.query.posts },
-        },
-        profile: {
-          create: { bio: req.query.profile },
-        },
-      },
-    });
-    res.status(200).send("Succesfully added user.");
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+app.get('/database/post/user',
+        async (req, res) => { /// ?name=alice&posts=hey&profile=bio
+          try {
+            await prisma.user.create({
+              data : {
+                name : req.query.name,
+                email : req.query.name + '@prisma.io',
+                posts : {
+                  create : {title : req.query.posts},
+                },
+                profile : {
+                  create : {bio : req.query.profile},
+                },
+              },
+            });
+            res.status(200).send("Succesfully added user.");
+          } catch (err) {
+            res.status(500).send(err);
+          }
+        });
 
 /// Update a data with Prisma example
-app.get('/database/update/user/name', async (req, res) => { /// ?id=1&name=pierre
-  try {
-    await prisma.user.update({
-      where: { id: Number(req.query.id) },
-      data: { name: req.query.name },
-    });
-    res.status(200).send("Succesfully updated user.");
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+app.get('/database/update/user/name',
+        async (req, res) => { /// ?id=1&name=pierre
+          try {
+            await prisma.user.update({
+              where : {id : Number(req.query.id)},
+              data : {name : req.query.name},
+            });
+            res.status(200).send("Succesfully updated user.");
+          } catch (err) {
+            res.status(500).send(err);
+          }
+        });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
-});
+app.listen(PORT, HOST,
+           () => { console.log(`Server running on http://${HOST}:${PORT}`); });
