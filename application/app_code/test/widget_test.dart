cb2831f01a108d60263977b32d0371a68b308314
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-18 13:50:58 +0100

    Fix Flutter code format for testing file

--- application/app_code/test/widget_test.dart
+++ application/app_code/test/widget_test.dart
@@ -4,14 +4,24 @@ import 'package:flutter_test/flutter_test.dart';
 
 void templateFunction() {
   testWidgets('Just a basic template test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp()); /// Launch the main page of the project
+    await tester.pumpWidget(const MyApp());
+
+    /// Launch the main page of the project
 
     expect(find.text('You have pushed the button this many times:'),
-        findsOneWidget); /// Find a text containing this
-    expect(find.text('1'), findsNothing); /// Expected no widget containing '1'
+        findsOneWidget);
 
-    await tester.tap(find.byIcon(Icons.add)); /// Click on a button containing the 'add' icon
-    await tester.pump(); /// Update the page (Do it after each action performing a set state.
+    /// Find a text containing this
+    expect(find.text('1'), findsNothing);
+
+    /// Expected no widget containing '1'
+
+    await tester.tap(find.byIcon(Icons.add));
+
+    /// Click on a button containing the 'add' icon
+    await tester.pump();
+
+    /// Update the page (Do it after each action performing a set state.
   });
 }
 
