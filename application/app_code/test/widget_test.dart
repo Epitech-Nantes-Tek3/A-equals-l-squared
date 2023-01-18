56c0c177dde13b2d271731a4c8926124f396d9c2
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-18 13:50:32 +0100

    Create a template function for Flutter testing

--- application/app_code/test/widget_test.dart
+++ application/app_code/test/widget_test.dart
@@ -2,17 +2,19 @@ import 'package:application/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+void templateFunction() {
+  testWidgets('Just a basic template test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp()); /// Launch the main page of the project
 
     expect(find.text('You have pushed the button this many times:'),
-        findsOneWidget);
-    expect(find.text('1'), findsNothing);
+        findsOneWidget); /// Find a text containing this
+    expect(find.text('1'), findsNothing); /// Expected no widget containing '1'
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    /// Click on a widget, wawww
+    await tester.tap(find.byIcon(Icons.add)); /// Click on a button containing the 'add' icon
+    await tester.pump(); /// Update the page (Do it after each action performing a set state.
   });
+}
+
+void main() {
+  templateFunction();
 }
