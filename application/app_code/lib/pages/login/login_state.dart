b0b7645b1342799112b1ea2a508a14d6deda198b
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-24 10:03:43 +0100

    Create a basic Login page architecture #33

---
+++ application/app_code/lib/pages/login/login_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Authentication page')));
+  }
+}
