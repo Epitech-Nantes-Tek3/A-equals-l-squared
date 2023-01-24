493bcc7324d8bc7761f696cf22d266fadb1351a0
Bunny <tomduvalmahe44@gmail.com>
2023-01-24 14:12:03 +0100

    Add documentation user class #34

--- application/app_code/lib/flutter_objects/user.dart
+++ application/app_code/lib/flutter_objects/user.dart
@@ -1,11 +1,13 @@
+/// This class is the User class.
+/// It contains all information about a User
 class User {
-
   final String userName;
   final String token;
   final bool isAdmin;
   final String email;
   final DateTime createdAt;
 
+  /// Constructor of the User class
   const User({
     required this.userName,
     required this.email,
@@ -13,6 +15,8 @@ class User {
     required this.isAdmin,
     required this.createdAt,
   });
+
+  /// Convert a json map into the class
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       userName: json['user']['username'],
