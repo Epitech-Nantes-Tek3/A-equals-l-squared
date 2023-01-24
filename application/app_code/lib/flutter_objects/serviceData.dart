65018563f8f8f43a5af4ca0be8ec32fdbbee331c
Bunny <tomduvalmahe44@gmail.com>
2023-01-24 15:38:35 +0100

    Refacto for Service Class to ServiceData class#34
    Refacto of Service class to ServiceData class because Service is a class which already exist in flutter

--- application/app_code/lib/flutter_objects/serviceData.dart
+++ application/app_code/lib/flutter_objects/serviceData.dart
@@ -1,20 +1,20 @@
 /// This class is the service class.
 /// It contains all information about a service
-class Service {
+class ServiceData {
   final String name;
   final String description;
   final DateTime createdAt;
 
   /// Constructor of the service class
-  const Service({
+  const ServiceData({
     required this.name,
     required this.description,
     required this.createdAt,
   });
 
   /// Convert a json map into the class
-  factory Service.fromJson(Map<String, dynamic> json) {
-    return Service(
+  factory ServiceData.fromJson(Map<String, dynamic> json) {
+    return ServiceData(
       name: json['service']['name'],
       description: json['service']['description'],
       createdAt: json["service"]["createdAt"],
