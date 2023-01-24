a5f29de82ab3d11a68c0c42928b0848f2dc986f8
Bunny <tomduvalmahe44@gmail.com>
2023-01-24 14:25:40 +0100

    Add reaction class #34
    Add reaction class for flutter object with a variable :
          final String name;
          final String description;
          final DateTime createdAt;
          final bool isEnable;

---
+++ application/app_code/lib/flutter_objects/reaction.dart
@@ -0,0 +1,26 @@
+/// This class is the reaction class.
+/// It contains all information about a reaction
+class Reaction {
+  final String name;
+  final String description;
+  final DateTime createdAt;
+  final bool isEnable;
+
+  /// Constructor of the reaction class
+  const Reaction({
+    required this.name,
+    required this.description,
+    required this.createdAt,
+    required this.isEnable,
+  });
+
+  /// Convert a json map into the class
+  factory Reaction.fromJson(Map<String, dynamic> json) {
+    return Reaction(
+        name: json['reaction']['name'],
+        description: json['reaction']['description'],
+        createdAt: json["reaction"]["createdAt"],
+        isEnable: json["reaction"]["isEnable"]
+    );
+  }
+}
