8c9e0694ac7ed272ece150769cf1bef95a5a67e3
Bunny <tomduvalmahe44@gmail.com>
2023-01-24 14:20:02 +0100

    Add service class #34
    Add service class for flutter object with a variable :
        final String service;

---
+++ application/app_code/lib/flutter_objects/service.dart
@@ -0,0 +1,23 @@
+/// This class is the service class.
+/// It contains all information about a service
+class Service {
+  final String name;
+  final String description;
+  final DateTime createdAt;
+
+  /// Constructor of the service class
+  const Service({
+    required this.name,
+    required this.description,
+    required this.createdAt,
+  });
+
+  /// Convert a json map into the class
+  factory Service.fromJson(Map<String, dynamic> json) {
+    return Service(
+        name: json['service']['name'],
+        description: json['service']['description'],
+        createdAt: json["service"]["createdAt"],
+    );
+  }
+}
