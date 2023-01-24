5d535c591217e680145b56969b9257060526e98d
Bunny <tomduvalmahe44@gmail.com>
2023-01-24 15:51:01 +0100

    Reformat fileName for #34

--- application/app_code/test/testsFlutterObjects/error_data_tests.dart
+++ application/app_code/test/testsFlutterObjects/error_data_tests.dart
@@ -1,4 +1,4 @@
-import 'package:application/flutter_objects/errorData.dart';
+import 'package:application/flutter_objects/errordata.dart';
 import 'package:application/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
--- application/app_code/test/testsFlutterObjects/user_data_tests.dart
+++ application/app_code/test/testsFlutterObjects/user_data_tests.dart
@@ -1,4 +1,4 @@
-import 'package:application/flutter_objects/userData.dart';
+import 'package:application/flutter_objects/userdata.dart';
 import 'package:application/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
