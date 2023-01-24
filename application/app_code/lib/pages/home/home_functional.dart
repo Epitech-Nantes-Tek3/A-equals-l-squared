923569bb02843df7940907caa49995f22fc1bc0c
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-24 13:56:13 +0100

    Add a way to securize road with auth #32

--- application/app_code/lib/pages/home/home_functional.dart
+++ application/app_code/lib/pages/home/home_functional.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../login/login_functional.dart';
+
 void goToHomePage(BuildContext context) {
+  if (!isAuth) {
+    context.go('/');
+    return;
+  }
   context.go('/home');
 }
--- application/app_code/lib/pages/login/login_functional.dart
+++ application/app_code/lib/pages/login/login_functional.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+bool isAuth = false;
+
 void goToLoginPage(BuildContext context) {
+  isAuth = false;
   context.go('/');
 }
