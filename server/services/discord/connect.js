2f15b3e05ae393ac179d2ed3cc2b5e8e314eba32
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-01-25 17:54:09 +0100

    Connect the Discord Bot with its token

---
+++ server/services/discord/connect.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { Client, Intents } = require('discord.js')
+const fetch = require('node-fetch')
+
+const client = new Client({
+  intents: [
+    Intents.Guilds,
+    Intents.GuildMessages,
+    Intents.MessageContent,
+    Intents.GuildMembers,
+    Intents.GuildMessageReactions,
+    Intents.DirectMessages
+  ],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+})
+
+client.login(process.env.DISCORD_TOKEN)
+
+client.once('ready', () => {
+  console.log('Available on servers: ')
+  console.log(
+    `${client.guilds.cache.map(guild => `\t'${guild.name}'\n`).join('')}`
+  )
+})
+
+module.exports = client
