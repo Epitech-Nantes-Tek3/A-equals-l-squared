d7d16f39873c011743874feb9e8bc7d0ae440107
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-01-25 17:56:16 +0100

    Send a message to a channel thanks to its ID

---
+++ server/services/discord/reactions/send_message_channel.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const client = require('../connect')
+
+/**
+ * @brief Send a message to a channel
+ * @param {*} channelID ID of the channel you want to send the message
+ * @param {*} message Message that you want to send
+ * @returns True if the message has been sent, false otherwise
+ */
+function sendMessageChannel (channelID, message) {
+  client.channels
+    .fetch(channelID)
+    .then(channel => {
+      channel.send(message)
+      console.log('Message sent successfully')
+      return true
+    })
+    .catch(err => {
+      console.error(err.message)
+      return false
+    })
+}
+
+module.exports = sendMessageChannel
