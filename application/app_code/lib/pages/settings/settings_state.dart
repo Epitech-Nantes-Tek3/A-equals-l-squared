eddeed74cb11ccd069e807f1dcc007508b9c0b23
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-26 15:32:54 +0100

    Implement update screen functionality in flutter side #54

--- application/app_code/lib/pages/settings/settings_state.dart
+++ application/app_code/lib/pages/settings/settings_state.dart
@@ -8,8 +8,50 @@ import 'package:http/http.dart' as http;
 import '../login/login_functional.dart';
 
 class SettingsPageState extends State<SettingsPage> {
+  /// username to update
+  String? _username = userInformation?.userName;
+
+  /// email to update
+  String? _email = userInformation?.email;
+
+  /// password to update
+  String? _password = "";
+
   /// future api answer
   late Future<String> _futureAnswer;
+
+  /// Network function calling the api to signup
+  Future<String> apiAskForUpdate() async {
+    if (_username == null || _email == null || _password == "") {
+      return 'Please fill all the field !';
+    }
+    final response = await http.post(
+      Uri.parse('http://$serverIp:8080/api/user/update'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${userInformation!.token}',
+      },
+      body: jsonEncode(<String, String>{
+        'username': _username!,
+        'email': _email!,
+        'password': _password!
+      }),
+    );
+
+    try {
+      if (response.statusCode == 200) {
+        userInformation?.userName = _username!;
+        userInformation?.email = _email!;
+      } else {
+        _username = userInformation?.userName;
+        _email = userInformation?.email;
+        _password = null;
+      }
+      return jsonDecode(response.body);
+    } catch (err) {
+      return 'Invalid user token. Please go back to login.';
+    }
+  }
 
   /// Network function calling the api to delete account
   Future<String> apiAskForDelete() async {
@@ -39,6 +81,62 @@ class SettingsPageState extends State<SettingsPage> {
     _futureAnswer = getAFirstApiAnswer();
   }
 
+  Widget userDataVisualization() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Username',
+          ),
+          initialValue: _username,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (String? value) {
+            if (value != null && value.length <= 4) {
+              return 'Username must be min 5 characters long.';
+            }
+            _username = value;
+            return null;
+          },
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'E-mail',
+          ),
+          initialValue: _email,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (String? value) {
+            if (value != null &&
+                !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                    .hasMatch(value)) {
+              return 'Must be a valid email.';
+            }
+            _email = value;
+            return null;
+          },
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Password',
+          ),
+          initialValue: _password,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (String? value) {
+            if (value != null && value.length <= 7) {
+              return 'Password must be min 8 characters long.';
+            }
+            _password = value;
+            return null;
+          },
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,6 +145,16 @@ class SettingsPageState extends State<SettingsPage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           const Text('Welcome to Settings page'),
+          userDataVisualization(),
+          ElevatedButton(
+            key: const Key('AskUpdateButton'),
+            onPressed: () {
+              setState(() {
+                _futureAnswer = apiAskForUpdate();
+              });
+            },
+            child: const Text('Update account information'),
+          ),
           ElevatedButton(
             key: const Key('AskDeleteButton'),
             onPressed: () {
