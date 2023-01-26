cd03824083dd3bdadac0ee54814cc480bfff4cd9
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-01-26 11:55:16 +0100

    Fix XML error for Flutter web #37

--- server/server_app.js
+++ server/server_app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE,PATCH')
   res.header('Access-Control-Allow-Credentials', true)
