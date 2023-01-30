78af8e70884c3f42b4d841074fe3215d6d87bfd2
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-01-30 12:45:49 +0100

    Add function to export Discord Service in the DB

--- server/services/discord/init.js
+++ server/services/discord/init.js
@@ -15,12 +15,12 @@ const client = new Client({
 })
 
 /**
- * Connect to the Discord bot thanks to its Token
+ * Connect to the Discord bot thanks to its Token.
  */
 client.login(process.env.DISCORD_TOKEN)
 
 /**
- * Called when the Discord bot is ready, here we display all the servers the bot is on
+ * Called when the Discord bot is ready, here we display all the servers the bot is on.
  */
 client.once('ready', () => {
   console.log('Available on servers: ')
@@ -29,4 +29,101 @@ client.once('ready', () => {
   )
 })
 
-module.exports = client
+/**
+ * @brief Create the Discord service in the database.
+ */
+const createDiscordService = async () => {
+  await database.prisma.Service.create({
+    data: {
+      name: 'Discord',
+      description: 'Discord service',
+      isEnable: true,
+      Actions: {
+        create: [
+          {
+            name: 'onMessage',
+            description: 'When a message is sent',
+            isEnable: true
+          },
+          {
+            name: 'onVoiceChannel',
+            description: 'When a user join a voice channel',
+            isEnable: true,
+            parameters: {
+              create: [
+                {
+                  name: 'channelId',
+                  description: 'The channel id where the user join',
+                  type: 'string'
+                }
+              ]
+            }
+          },
+          {
+            name: 'onReactionAdd',
+            description: 'When a user add a reaction to a message',
+            isEnable: true
+          }
+        ]
+      },
+      Reactions: {
+        create: [
+          {
+            name: 'sendMessageChannel',
+            description: 'Send a message on a channel',
+            isEnable: true,
+            parameters: {
+              create: [
+                {
+                  name: 'messageContent',
+                  description: 'The content of the message',
+                  type: 'string'
+                },
+                {
+                  name: 'channelId',
+                  description: 'The channel id where the message will be send',
+                  type: 'string'
+                }
+              ]
+            }
+          },
+          {
+            name: 'sendMessageUser',
+            description: 'Send a message to a user',
+            isEnable: true,
+            parameters: {
+              create: [
+                {
+                  name: 'messageContent',
+                  description: 'The content of the message',
+                  type: 'string'
+                },
+                {
+                  name: 'UserId',
+                  description: 'The user id where the message will be send',
+                  type: 'string'
+                }
+              ]
+            }
+          },
+          {
+            name: 'changeActivity',
+            description: 'Change the activity of the User',
+            isEnable: true,
+            parameters: {
+              create: [
+                {
+                  name: 'activity',
+                  description: 'The activity of the user',
+                  type: 'string'
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  })
+}
+
+module.exports = { client, createDiscordService }
