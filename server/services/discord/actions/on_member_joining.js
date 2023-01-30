7fb617935be5251cb442a8ab37dc91f0f06dff37
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-01-30 11:55:17 +0100

    Add Discord action when member joining a server

---
+++ server/services/discord/actions/on_member_joining.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const client = require('../connect')
+
+/**
+ * @brief Triggered when a new user joined the guild.
+ * @param {*} user User object
+ */
+client.on('guildMemberAdd', user => {
+  console.log('A new user just joined the Discord server: ' + user.tag)
+})
