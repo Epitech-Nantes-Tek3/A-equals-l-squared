fb1b27e3d2d2fa191fcc66b45e5bf33ea8d2aaa2
Bunny <tomduvalmahe44@gmail.com>
2023-01-31 11:56:08 +0100

    Lint format #65

--- application/app_code/lib/material_lib_functions/material_functions.dart
+++ application/app_code/lib/material_lib_functions/material_functions.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
-  /// This function display our logo
-  Widget displayLogo(double size) {
-    return Icon(size: size, Icons.apple);
-  }
+/// This function display our logo
+Widget displayLogo(double size) {
+  return Icon(size: size, Icons.apple);
+}
 
-  /// Add our shadow on a Widget
-  Widget materialShadowForArea(Widget widget) {
-    return Material(elevation: 5, shadowColor: Colors.black, child: widget);
-  }
+/// Add our shadow on a Widget
+Widget materialShadowForArea(Widget widget) {
+  return Material(elevation: 5, shadowColor: Colors.black, child: widget);
+}
 
-  /// Return our blue color Hex : 06A1E4
-  Color getOurBlueAreaColor(double opacity) {
-    return Color.fromRGBO(6, 161, 228, opacity);
-  }
+/// Return our blue color Hex : 06A1E4
+Color getOurBlueAreaColor(double opacity) {
+  return Color.fromRGBO(6, 161, 228, opacity);
+}
 
-  /// Return our green color Hex : 16FCBC
-  Color getOurGreenAreaColor(double opacity) {
-    return Color.fromRGBO(22, 252, 188, opacity);
-  }
+/// Return our green color Hex : 16FCBC
+Color getOurGreenAreaColor(double opacity) {
+  return Color.fromRGBO(22, 252, 188, opacity);
+}
