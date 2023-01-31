1498975f3f7bb33fdfe47e80f56f6706ee547f5f
Bunny <tomduvalmahe44@gmail.com>
2023-01-31 12:05:31 +0100

    Change parameter of Icon #65
    Because Lucas is deprecated

--- application/app_code/lib/material_lib_functions/material_functions.dart
+++ application/app_code/lib/material_lib_functions/material_functions.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 /// This function display our logo
 Widget displayLogo(double size) {
-  return Icon(size: size, Icons.apple);
+  return Icon(Icons.apple, size: size);
 }
 
 /// Add our shadow on a Widget
