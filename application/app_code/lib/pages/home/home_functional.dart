224c502f35958b926fdfbcc1fd1c2fb46e9fff65
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-02-02 09:27:47 +0100

    Ask the server to get all the services #75

--- application/app_code/lib/pages/home/home_functional.dart
+++ application/app_code/lib/pages/home/home_functional.dart
@@ -1,5 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
+
+import '../../flutter_objects/service_data.dart';
+import '../../network/informations.dart';
+
+/// List of all the Flutter data, represented by the services
+List<ServiceData> serviceDataList = <ServiceData>[];
 
 /// Navigation function -> Go to Home page
 void goToHomePage(BuildContext context) {
@@ -9,5 +18,24 @@ void goToHomePage(BuildContext context) {
 
 /// Update all the Flutter object and call the api
 void updateAllFlutterObject(BuildContext context) async {
-  print('Update');
+  try {
+    final response = await http.get(
+      Uri.parse('http://$serverIp:8080/api/get/service'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${userInformation!.token}',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<ServiceData> newList = <ServiceData>[];
+      var json = jsonDecode(response.body);
+      for (var temp in json['data']['services']) {
+        newList.add(ServiceData.fromJson(temp));
+      }
+      serviceDataList = newList;
+    }
+  } catch (err) {
+    debugPrint(err.toString());
+  }
 }
