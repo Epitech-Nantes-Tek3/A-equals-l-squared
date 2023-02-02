2a90c0f80c4a20f7bb1dec533bb9cff9b534312a
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-02-02 20:53:11 +0100

    Add Service display inside service list page #75

--- application/app_code/lib/pages/service_list/service_list_state.dart
+++ application/app_code/lib/pages/service_list/service_list_state.dart
@@ -4,6 +4,19 @@ import 'package:flutter/material.dart';
 import '../home/home_functional.dart';
 
 class ServiceListPageState extends State<ServiceListPage> {
+  /// Display all the service
+  Widget displayAllService() {
+    List<Widget> serviceVis = <Widget>[];
+
+    for (var temp in serviceDataList) {
+      serviceVis.add(temp.display());
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: serviceVis,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +25,7 @@ class ServiceListPageState extends State<ServiceListPage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           const Text('Welcome to Service List page'),
+          displayAllService(),
           ElevatedButton(
             key: const Key('ServiceListHomeButton'),
             onPressed: () {
