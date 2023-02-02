06338cfbc39c8549c192d606f0e67138c205f17e
Quentin C. | ZQUEMA <zquema1@gmail.com>
2023-02-02 17:43:15 +0100

    Add logs on creation of the 'Discord' service

--- server/services/discord/init.js
+++ server/services/discord/init.js
@@ -33,97 +33,103 @@ client.once('ready', () => {
  * @brief Create the Discord service in the database.
  */
 const createDiscordService = async () => {
-  await database.prisma.Service.create({
-    data: {
-      name: 'Discord',
-      description: 'Discord service',
-      isEnable: true,
-      Actions: {
-        create: [
-          {
-            name: 'onMessage',
-            description: 'When a message is sent',
-            isEnable: true
-          },
-          {
-            name: 'onVoiceChannel',
-            description: 'When a user join a voice channel',
-            isEnable: true,
-            parameters: {
-              create: [
-                {
-                  name: 'channelId',
-                  description: 'The channel id where the user join',
-                  type: 'string'
-                }
-              ]
+  try {
+    const discord = await database.prisma.Service.create({
+      data: {
+        name: 'Discord',
+        description: 'Discord service',
+        isEnable: true,
+        Actions: {
+          create: [
+            {
+              name: 'onMessage',
+              description: 'When a message is sent',
+              isEnable: true
+            },
+            {
+              name: 'onVoiceChannel',
+              description: 'When a user join a voice channel',
+              isEnable: true,
+              parameters: {
+                create: [
+                  {
+                    name: 'channelId',
+                    description: 'The channel id where the user join',
+                    displayName: 'string'
+                  }
+                ]
+              }
+            },
+            {
+              name: 'onReactionAdd',
+              description: 'When a user add a reaction to a message',
+              isEnable: true
             }
-          },
-          {
-            name: 'onReactionAdd',
-            description: 'When a user add a reaction to a message',
-            isEnable: true
-          }
-        ]
-      },
-      Reactions: {
-        create: [
-          {
-            name: 'sendMessageChannel',
-            description: 'Send a message on a channel',
-            isEnable: true,
-            parameters: {
-              create: [
-                {
-                  name: 'messageContent',
-                  description: 'The content of the message',
-                  type: 'string'
-                },
-                {
-                  name: 'channelId',
-                  description: 'The channel id where the message will be send',
-                  type: 'string'
-                }
-              ]
+          ]
+        },
+        Reactions: {
+          create: [
+            {
+              name: 'sendMessageChannel',
+              description: 'Send a message on a channel',
+              isEnable: true,
+              parameters: {
+                create: [
+                  {
+                    name: 'messageContent',
+                    description: 'The content of the message',
+                    displayName: 'string'
+                  },
+                  {
+                    name: 'channelId',
+                    description:
+                      'The channel id where the message will be send',
+                    displayName: 'string'
+                  }
+                ]
+              }
+            },
+            {
+              name: 'sendMessageUser',
+              description: 'Send a message to a user',
+              isEnable: true,
+              parameters: {
+                create: [
+                  {
+                    name: 'messageContent',
+                    description: 'The content of the message',
+                    displayName: 'string'
+                  },
+                  {
+                    name: 'UserId',
+                    description: 'The user id where the message will be send',
+                    displayName: 'string'
+                  }
+                ]
+              }
+            },
+            {
+              name: 'changeActivity',
+              description: 'Change the activity of the User',
+              isEnable: true,
+              parameters: {
+                create: [
+                  {
+                    name: 'activity',
+                    description: 'The activity of the user',
+                    displayName: 'string'
+                  }
+                ]
+              }
             }
-          },
-          {
-            name: 'sendMessageUser',
-            description: 'Send a message to a user',
-            isEnable: true,
-            parameters: {
-              create: [
-                {
-                  name: 'messageContent',
-                  description: 'The content of the message',
-                  type: 'string'
-                },
-                {
-                  name: 'UserId',
-                  description: 'The user id where the message will be send',
-                  type: 'string'
-                }
-              ]
-            }
-          },
-          {
-            name: 'changeActivity',
-            description: 'Change the activity of the User',
-            isEnable: true,
-            parameters: {
-              create: [
-                {
-                  name: 'activity',
-                  description: 'The activity of the user',
-                  type: 'string'
-                }
-              ]
-            }
-          }
-        ]
+          ]
+        }
       }
-    }
-  })
+    })
+    console.log('Discord service created : ', discord)
+  } catch (error) {
+    console.log('Error while creating Discord service : ', error)
+  }
 }
 
 module.exports = { client, createDiscordService }
