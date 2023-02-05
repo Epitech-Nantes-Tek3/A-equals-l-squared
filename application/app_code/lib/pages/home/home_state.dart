297168cc0da05d00e88f66e81eff5823e5089cfb
Bunny <tomduvalmahe44@gmail.com>
2023-02-05 12:20:59 +0100

    Change color behind list of Areas #78
    Change primary color of elevated buttons

--- application/app_code/lib/pages/home/home_state.dart
+++ application/app_code/lib/pages/home/home_state.dart
@@ -28,10 +28,8 @@ class HomePageState extends State<HomePage> {
       areaVis.add(ElevatedButton(
           style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            side: const BorderSide(width: 3, color: Colors.white),
-
-            /// Change when DB is Up
-            backgroundColor: const Color.fromRGBO(255, 255, 255, 0),
+            side: const BorderSide(width: 3, color: Colors.blue), /// Change when DB is Up
+            primary: Colors.white, /// Not deprecated
           ),
           onPressed: () {
             updatingArea = temp;
