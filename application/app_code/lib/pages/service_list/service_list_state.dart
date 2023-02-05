3666e38e73678800df6718e6a89865adac7c6339
Bunny <tomduvalmahe44@gmail.com>
2023-02-05 18:26:32 +0100

    Modified Service List #78

--- application/app_code/lib/pages/service_list/service_list_state.dart
+++ application/app_code/lib/pages/service_list/service_list_state.dart
@@ -12,12 +12,22 @@ class ServiceListPageState extends State<ServiceListPage> {
 
     for (var temp in serviceDataList) {
       serviceVis.add(ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            side: const BorderSide(width: 3, color: Colors.white),
+
+            /// Change when DB is Up
+            primary: Colors.white,
+
+            /// Not deprecated
+          ),
           onPressed: () {
             createdAreaContent = <ServiceData>[];
             createdAreaContent.add(temp);
             goToCreateAreaPage(context);
           },
           child: temp.display()));
+      serviceVis.add(const SizedBox(height: 10,));
     }
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -28,23 +38,33 @@ class ServiceListPageState extends State<ServiceListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+        body: SingleChildScrollView(
+            child: Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          const Text('Welcome to Service List page'),
-          displayAllService(),
-          ElevatedButton(
-            key: const Key('ServiceListHomeButton'),
-            onPressed: () {
-              setState(() {
-                goToHomePage(context);
-              });
-            },
-            child: const Text('Go Home'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      goToHomePage(context);
+                    });
+                  },
+                  icon: const Icon(Icons.home_filled)),
+              const Text(
+                'Service List page',
+                style: TextStyle(fontFamily: 'Roboto-Bold', fontSize: 25),
+              )
+            ],
           ),
+          const Text('All our implemented Services, if you want to create something with one of these, click on it !'),
+          const SizedBox(height: 20,),
+          displayAllService(),
         ],
       ),
-    ));
+    )));
   }
 }
