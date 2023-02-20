62158734d604b6470be076765e76ca75213324d1
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-02-20 17:01:10 +0100

    Add initialisation in the DB of Deezer service #117

--- server/services/deezer/init.js
+++ server/services/deezer/init.js
@@ -1,2 +1,63 @@
+'use strict'
+
 const database = require('../../database_init')
-const deezer = require('node-deezer')
+
+/**
+ * @brief Create the Deezer service in the database.
+ */
+const createDeezerService = async () => {
+  try {
+    const deezer = await database.prisma.Service.create({
+      data: {
+        name: 'Deezer',
+        description: 'Deezer service',
+        primaryColor: '#323232',
+        secondaryColor: '#EF5466',
+        icon: '',
+        isEnable: true,
+        Actions: {
+          create: [
+            {
+              name: '',
+              code: 'DZR-01',
+              description: '',
+              isEnable: false,
+              Parameters: {
+                create: [
+                  {
+                    name: '',
+                    description: '',
+                    isRequired: true,
+                    GetterUrl: ''
+                  }
+                ]
+              }
+            }
+          ]
+        },
+        Reactions: {
+          create: [
+            {
+              name: 'createPlaylist',
+              code: 'DZR-01',
+              description: 'Create a new empty playlist',
+              isEnable: true,
+              Parameters: {
+                create: [
+                  {
+                    name: 'name',
+                    description: 'The guild concerned guild id',
+                    isRequired: true
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    })
+    console.log('Deezer service created : ', deezer)
+  } catch (error) {
+    console.log('Error while creating Deezer service : ', error)
+  }
+}
