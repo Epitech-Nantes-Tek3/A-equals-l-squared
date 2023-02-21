e071ed8fbc550ca7607f71f00c210d8e3d0aae33
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-02-21 14:58:30 +0100

    Add a function that adds tracks to a Deezer playlist #117

---
+++ server/services/deezer/common/add_tracks_to_playlist.js
@@ -0,0 +1,20 @@
+'use strict'
+
+async function addTracksToPlaylist (playlistId, tracksId, deezerToken) {
+  try {
+    const url = `https://api.deezer.com/playlist/${playlistId}/tracks&songs=${tracksId.join(
+      '%2C'
+    )}&request_method=POST&access_token=${deezerToken}`
+    const response = await axios.get(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    return true
+  } catch (err) {
+    console.error(err.message)
+    return false
+  }
+}
+
+module.exports = { addTracksToPlaylist }
