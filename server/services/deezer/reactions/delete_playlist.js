8f39a0f9bf0cbcbdc2774bdb0982cabcb74b0bc7
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-02-21 14:56:56 +0100

    Add reaction of DeletePlaylist on Deezer #117

---
+++ server/services/deezer/reactions/delete_playlist.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { replaceDynamicParameters } = require('../../glue/dynamic_parameters.js')
+
+function deezerDeletePlaylistFromArea (Area, dynamicParameters) {
+  const reactionParameters = Area.ReactionParameters
+  let playlistId = reactionParameters.find(
+    parameter => parameter.Parameter.name == 'playlistId'
+  ).value
+  playlistId = replaceDynamicParameters(playlistId, dynamicParameters)
+  return createPlaylist(playlistId, Area.User.deezerToken)
+}
+
+async function deletePlaylist (playlistId, deezerToken) {
+  try {
+    const url = `https://api.deezer.com/playlist/${playlistId}&access_token=${deezerToken}`
+    const response_post = await axios.delete(url)
+  } catch (err) {
+    console.error(err.message)
+    return false
+  }
+}
+
+module.exports = { deezerDeletePlaylistFromArea, deletePlaylist }
