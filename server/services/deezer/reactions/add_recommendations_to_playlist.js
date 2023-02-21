c5c80a95bc4282b09d3eb56e3c8642632fd85cd6
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2023-02-21 14:57:53 +0100

    Add reaction of AddRecommendationsToPlaylist on Deezer #117

---
+++ server/services/deezer/reactions/add_recommendations_to_playlist.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { replaceDynamicParameters } = require('../../glue/dynamic_parameters.js')
+const { addTracksToPlaylist } = require('../common/add_tracks_to_playlist.js')
+
+async function deezerAddRecommendationsToPlaylistFromArea (
+  Area,
+  dynamicParameters
+) {
+  const reactionParameters = Area.ReactionParameters
+  let playlistId = reactionParameters.find(
+    parameter => parameter.Parameter.name == 'playlistId'
+  ).value
+  playlistId = replaceDynamicParameters(playlistId, dynamicParameters)
+  return addRecommendationsToPlaylist(playlistId, Area.User.deezerToken)
+}
+
+async function addRecommendationsToPlaylist (playlistId, deezerId, deezerToken) {
+  try {
+    const url = `https://api.deezer.com/user/${deezerId}/history&access_token=${deezerToken}`
+    const response = await axios.get(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const tracks = response.data.data
+    const tracksId = []
+    tracks.map(track => tracksId.push(track.id))
+    addTracksToPlaylist(playlistId, tracksId, deezerToken)
+    return tracksId
+  } catch (err) {
+    console.error(err.message)
+    return false
+  }
+}
+
+module.exports = {
+  deezerAddRecommendationsToPlaylistFromArea,
+  addRecommendationsToPlaylist
+}
