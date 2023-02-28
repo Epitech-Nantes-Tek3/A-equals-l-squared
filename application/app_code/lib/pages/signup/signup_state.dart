0dd7c974e0c0df99c062ab234cfed60fb45c5b75
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-02-28 09:56:58 +0100

    Add tests setup for Signup page #158

--- application/app_code/lib/pages/signup/signup_state.dart
+++ application/app_code/lib/pages/signup/signup_state.dart
@@ -74,94 +74,98 @@ class SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Center(
-            child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        displayLogoAndName(),
-                        const Text(
-                          'Welcome to Signup page !',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 22),
-                        ),
-                        getHostConfigField(),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Username',
-                          ),
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (String? value) {
-                            if (value != null && value.length <= 4) {
-                              return 'Username must be min 5 characters long.';
-                            }
-                            _username = value;
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'E-mail',
-                          ),
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (String? value) {
-                            if (value != null &&
-                                !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                    .hasMatch(value)) {
-                              return 'Must be a valid email.';
-                            }
-                            _email = value;
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                          ),
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (String? value) {
-                            if (value != null && value.length <= 7) {
-                              return 'Password must be min 8 characters long.';
-                            }
-                            _password = value;
-                            return null;
-                          },
-                        ),
-                        FutureBuilder<String>(
-                          future: _futureSignup,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              return Text(snapshot.data!);
-                            } else if (snapshot.hasError) {
-                              return Text('${snapshot.error}');
-                            }
-                            return const CircularProgressIndicator();
-                          },
-                        ),
-                        ElevatedButton(
-                          key: const Key('SendSignupButton'),
-                          onPressed: () {
-                            setState(() {
-                              _futureSignup = apiAskForSignup();
-                            });
-                          },
-                          child: const Text('Signup'),
-                        ),
-                        TextButton(
-                          key: const Key('GoLoginButton'),
-                          onPressed: () {
-                            goToLoginPage(context);
-                          },
-                          child: const Text('Back to login screen...'),
-                        ),
-                      ],
-                    )))));
+        body: SingleChildScrollView(
+            child: Center(
+                child: Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 30),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            displayLogoAndName(),
+                            const Text(
+                              'Welcome to Signup page !',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 22),
+                            ),
+                            getHostConfigField(),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Username',
+                              ),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (String? value) {
+                                if (value != null && value.length <= 4) {
+                                  return 'Username must be min 5 characters long.';
+                                }
+                                _username = value;
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'E-mail',
+                              ),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (String? value) {
+                                if (value != null &&
+                                    !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                        .hasMatch(value)) {
+                                  return 'Must be a valid email.';
+                                }
+                                _email = value;
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Password',
+                              ),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (String? value) {
+                                if (value != null && value.length <= 7) {
+                                  return 'Password must be min 8 characters long.';
+                                }
+                                _password = value;
+                                return null;
+                              },
+                            ),
+                            FutureBuilder<String>(
+                              future: _futureSignup,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return Text(snapshot.data!);
+                                } else if (snapshot.hasError) {
+                                  return Text('${snapshot.error}');
+                                }
+                                return const CircularProgressIndicator();
+                              },
+                            ),
+                            ElevatedButton(
+                              key: const Key('SendSignupButton'),
+                              onPressed: () {
+                                setState(() {
+                                  _futureSignup = apiAskForSignup();
+                                });
+                              },
+                              child: const Text('Signup'),
+                            ),
+                            TextButton(
+                              key: const Key('GoLoginButton'),
+                              onPressed: () {
+                                goToLoginPage(context);
+                              },
+                              child: const Text('Back to login screen...'),
+                            ),
+                          ],
+                        ))))));
   }
 }
