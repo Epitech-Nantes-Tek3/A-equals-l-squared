d0ca8dc713fa23b3cc20d28a5095be7cf092505c
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-02-28 10:20:02 +0100

    Add tests setup for Settings page #158

--- application/app_code/test/testsPages/settings_tests.dart
+++ application/app_code/test/testsPages/settings_tests.dart
@@ -1,6 +1,25 @@
+import 'package:application/network/informations.dart';
+import 'package:application/pages/settings/settings_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void settingsPageTest() {
-  testWidgets(
-      'Some test with the settings page', (WidgetTester tester) async {});
+  testWidgets('Some test with the settings page', (WidgetTester tester) async {
+    userInformation = null;
+    await tester.pumpWidget(
+      MaterialApp(
+        title: 'Testing app',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: const SettingsPage(),
+      ),
+    );
+
+    await tester.pump();
+    expect(find.text("Settings Page"), findsOneWidget);
+
+    /// ADD NEW TEST HERE
+  });
 }
