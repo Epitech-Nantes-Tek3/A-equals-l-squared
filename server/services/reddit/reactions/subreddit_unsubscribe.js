dfb046a4669c0ee1a85d09629eb54021e4606324
tbellicha <tanguy.bellicha@epitech.eu>
2023-02-28 14:15:00 +0100

    Add unsubscribeToSubreddit function #153

---
+++ server/services/reddit/reactions/subreddit_unsubscribe.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const axios = require('axios')
+const { replaceDynamicParameters } = require('../../glue/dynamic_parameters.js')
+
+/**
+ * @brief Unsubscribe to a subreddit from an area
+ * @param {*} Area Area that contains the parameters
+ * @param {*} dynamicParameters Dynamic parameters
+ * @returns True if it was successful, false otherwise
+ */
+function redditUnsubscribeToSubredditFromArea (Area, dynamicParameters) {
+  const reactionParameters = Area.ReactionParameters
+  let subredditName = reactionParameters.find(
+    parameter => parameter.Parameter.name == 'subredditName'
+  ).value
+  subredditName = replaceDynamicParameters(subredditName, dynamicParameters)
+  return unsubscribeToSubreddit(Area.User.redditToken, subredditName)
+}
+
+/**
+ * @brief Unsubscribe to a subreddit
+ * @param {*} token Reddit token of the user
+ * @param {*} subredditName Name of the subreddit
+ * @returns True if it was successful, false otherwise
+ */
+async function unsubscribeToSubreddit (token, subredditName) {
+  try {
+    const response = await axios.post(
+      `https://oauth.reddit.com/api/subscribe`,
+      {
+        action: 'unsub',
+        sr_name: subredditName,
+        api_type: 'json'
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+    )
+    return true
+  } catch (error) {
+    console.log(error.data)
+    return false
+  }
+}
+
+module.exports = {
+  redditUnsubscribeToSubredditFromArea,
+  unsubscribeToSubreddit
+}
