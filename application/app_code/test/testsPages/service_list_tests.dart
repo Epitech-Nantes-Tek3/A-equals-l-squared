caffdd2654c3eb4cc3c30ff87bf568d1b15fae16
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-02-28 10:33:34 +0100

    Add tests setup for ServiceList page #158

--- application/app_code/test/testsPages/service_list_tests.dart
+++ application/app_code/test/testsPages/service_list_tests.dart
@@ -1,6 +1,26 @@
+import 'package:application/network/informations.dart';
+import 'package:application/pages/service_list/service_list_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void serviceListPageTest() {
-  testWidgets(
-      'Some test with the service list page', (WidgetTester tester) async {});
+  testWidgets('Some test with the service list page',
+      (WidgetTester tester) async {
+    userInformation = null;
+    await tester.pumpWidget(
+      MaterialApp(
+        title: 'Testing app',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: const ServiceListPage(),
+      ),
+    );
+
+    await tester.pump();
+    expect(find.text("Service List page"), findsOneWidget);
+
+    /// ADD NEW TEST HERE
+  });
 }
