e1876945f01dcec649d8d5844164be8ede526054
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-02-28 10:37:51 +0100

    Add tests setup for createArea page #158

--- application/app_code/test/testsPages/create_area_tests.dart
+++ application/app_code/test/testsPages/create_area_tests.dart
@@ -1,6 +1,26 @@
+import 'package:application/network/informations.dart';
+import 'package:application/pages/create_area/create_area_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void createAreaPageTest() {
   testWidgets(
-      'Some test with the create Area page', (WidgetTester tester) async {});
+      'Some test with the create Area page', (WidgetTester tester) async {
+    userInformation = null;
+    await tester.pumpWidget(
+      MaterialApp(
+        title: 'Testing app',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: const CreateAreaPage(),
+      ),
+    );
+
+    await tester.pump();
+    expect(find.text(''), findsWidgets);
+
+    /// ADD NEW TEST HERE
+  });
 }
