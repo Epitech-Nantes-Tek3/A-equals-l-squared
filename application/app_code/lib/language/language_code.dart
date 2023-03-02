14f36ae62e1c7bea3a9f3aebb34ec731c0f0cfd0
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-03-02 15:15:36 +0100

    Add english translation for actionData file #159

--- application/app_code/lib/language/language_code.dart
+++ application/app_code/lib/language/language_code.dart
@@ -3,3 +3,7 @@
 /// Language File :
 
 // EXCEPT-01 -> A basic Exception text returned in case of missing translation.
+
+/// Action Data File :
+
+// ACT-01 -> Title of the dynamic parameter part inside an action
--- application/app_code/lib/language/language_english.dart
+++ application/app_code/lib/language/language_english.dart
@@ -1,3 +1,3 @@
 /// Global map storing translated string in english
 Map<String, String> englishLanguageMap =
-Map.unmodifiable({'EXCEPT-01': 'EXCEPTION'});
+    Map.unmodifiable({'EXCEPT-01': 'EXCEPTION', 'ACT-01': "Dynamic parameter"});
