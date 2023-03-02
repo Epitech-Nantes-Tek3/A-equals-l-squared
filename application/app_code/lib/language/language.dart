f9ec0ef8959d915cf148b9ec5902f1e3a2256e9f
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-03-02 14:43:17 +0100

    Add languageMap variable #159

--- application/app_code/lib/language/language.dart
+++ application/app_code/lib/language/language.dart
@@ -4,6 +4,10 @@ List<String> availableLanguage = ["Francais", "English", "Española", "Deutsch"]
 /// Current selected language
 String selectedLanguage = "English";
 
+/// Global map storing translated string in all language
+Map<String, dynamic> languageMap = Map.unmodifiable(
+    {'Francais': 1, "English": 2, "Española": 3, "Deutsch": 4});
+
 /// Utility function returning a String in the selected language by her code.
 /// All the codes are listed in the file: language_code.dart
 /// And their equivalent in the different language files of this folder
