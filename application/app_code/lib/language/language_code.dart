30a34a0a289547611dc2b07cb9e60b9ce2deb875
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-03-02 15:28:56 +0100

    Add english translation for areaData file #159

--- application/app_code/lib/language/language_code.dart
+++ application/app_code/lib/language/language_code.dart
@@ -7,3 +7,15 @@
 /// Action Data File :
 
 // ACT-01 -> Title of the dynamic parameter part inside an action
+
+/// Area Data File :
+
+// AREA-01 -> Title of the Description preview part inside an area
+// AREA-02 -> Content of the Description preview when none provided inside an area
+// AREA-03 -> Title of the Action part inside an area display
+// AREA-04 -> Title of the Reaction part inside an area display
+// AREA-05-01 -> First part of sentence representing an area status in customisation display of area
+// AREA-05-02 -> Second part of sentence representing an area status (Activated) in customisation display of area
+// AREA-05-03 -> Second part of sentence representing an area status (Disabled) in customisation display of area
+// AREA-06 -> Error message displayed in case of an old JWT token in area customization display
+// AREA-07 -> Description text displayed when no description is provided by the area object
--- application/app_code/lib/language/language_english.dart
+++ application/app_code/lib/language/language_english.dart
@@ -1,3 +1,14 @@
 /// Global map storing translated string in english
-Map<String, String> englishLanguageMap =
-    Map.unmodifiable({'EXCEPT-01': 'EXCEPTION', 'ACT-01': "Dynamic parameter"});
+Map<String, String> englishLanguageMap = Map.unmodifiable({
+  'EXCEPT-01': 'No translation available...',
+  'ACT-01': 'Dynamic parameter',
+  'AREA-01': 'Description : \n\n ',
+  'AREA-02': 'No description',
+  'AREA-03': 'Actions',
+  'AREA-04': 'Reactions',
+  'AREA-05-01': 'Area : ',
+  'AREA-05-02': ' is activated',
+  'AREA-05-03': ' is disabled',
+  'AREA-06': 'Please logout and login.',
+  'AREA-07': 'No description'
+});
