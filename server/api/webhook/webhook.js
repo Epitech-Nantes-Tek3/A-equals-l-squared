c13e7e9603026266bc13a6bd1701d37fc5a85896
ZQUEMA <quentin.camilleri@epitech.eu>
2023-03-02 15:40:05 +0100

    Webhook generator #160

--- server/api/webhook/webhook.js
+++ server/api/webhook/webhook.js
@@ -20,6 +20,66 @@ function get_ngrok_url () {
 module.exports = function (app, passport, db) {
   const ngrok_url = get_ngrok_url()
 
+  const generate_webhook = (userId, serviceName, actionName, actionLinkId) => {
+    return (
+      ngrok_url +
+      '/api/webhook/' +
+      userId +
+      '/' +
+      serviceName +
+      '/' +
+      actionName +
+      '/' +
+      actionLinkId)
+  }
+
+  app.post(
+    '/api/webhook/generate',
+    passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+      try {
+        console.log('New webhook generator call.')
+        if (
+          !('userId' in req.body) ||
+          !req.body.userId ||
+          !('actionLinkId' in req.body) ||
+          !req.body.actionLinkId
+        ) {
+          return res.status(400).send('Bad request')
+        }
+        const actionLink = await db.prisma.AREAhasActions.findMany({
+          where: {
+            id: req.body.actionLinkId
+          },
+          select: {
+            Action: {
+              select: {
+                name: true,
+                Service: {
+                  select: {
+                    name: true
+                  }
+                }
+              }
+            }
+          }
+        })
+        if (actionLink.length === 0) {
+          return res.status(400).send('Bad request')
+        }
+        const webhook = generate_webhook(
+          req.body.userId,
+          actionLink.Action.Service.name,
+          actionLink.Action.name,
+          req.body.actionLinkId
+        )
+        return res.status(200).send(webhook)
+      } catch (error) {
+        console.log('Webhooks generator error', error)
+        return res.status(500).send('Internal server error')
+      }
+    }
+  )
 
   app.post('/api/webhook', function (req, res) {
     try {
