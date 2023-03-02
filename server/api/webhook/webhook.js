093b110e2942db9d6f48792066e1d5bda632916a
ZQUEMA <quentin.camilleri@epitech.eu>
2023-03-02 15:02:57 +0100

    Fetch the NGROK public URL #160

--- server/api/webhook/webhook.js
+++ server/api/webhook/webhook.js
@@ -1,10 +1,34 @@
 'use strict'
 
-module.exports = function (app, passport, database) {
-  // Webhook
+const axios = require('axios')
+
+function get_ngrok_url () {
+  try {
+    axios
+      .get('http://ngrok:4040/api/tunnels')
+      .then(response => {
+        return response.data['tunnels'][0]['public_url']
+      })
+      .catch(error => {
+        console.error('4040/api/tunnels data', error)
+      })
+  } catch (error) {
+    console.log('get URL error', error)
+  }
+}
+
+module.exports = function (app, passport, db) {
+  const ngrok_url = get_ngrok_url()
+
+
   app.post('/api/webhook', function (req, res) {
-    console.log('Webhook received')
-    console.log(req.body)
+    try {
+      console.log('Webhook received new function call.')
+      console.log(req.body)
+      console.log('My ngrok url is: ', ngrok_url)
+    } catch (error) {
+      console.log('Weebhooks error', error)
+    }
     res.status(200).send('OK')
   })
 }
