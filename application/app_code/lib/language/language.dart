b4131a5484e02ae3b7bde8ee6af472a54d02dfe1
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-03-02 14:38:31 +0100

    Add selected language variable #159

--- application/app_code/lib/language/language.dart
+++ application/app_code/lib/language/language.dart
@@ -1,6 +1,9 @@
 /// List of all available language in the application
 List<String> availableLanguage = ["Francais", "English", "Española", "Deutsch"];
 
+/// Current selected language
+String selectedLanguage = "English";
+
 /// Utility function returning a String in the selected language by her code.
 /// All the codes are listed in the file: language_code.dart
 /// And their equivalent in the different language files of this folder
