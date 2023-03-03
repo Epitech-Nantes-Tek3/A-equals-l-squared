7229fcd88350ad0a606e173b069271aeb96c2cb6
Bunny <tomduvalmahe44@gmail.com>
2023-03-03 15:50:17 +0100

    Improve Front-End #113
     - Lint format

--- application/app_code/lib/material_lib_functions/material_functions.dart
+++ application/app_code/lib/material_lib_functions/material_functions.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 /// This function display our logo
 Widget displayLogo(double size) {
   return Column(
-    children: <Widget>[Image.asset('assets/icons/Area_Logo.png', height: size, width: size,),],
+    children: <Widget>[
+      Image.asset(
+        'assets/icons/Area_Logo.png',
+        height: size,
+        width: size,
+      ),
+    ],
   );
 }
 
