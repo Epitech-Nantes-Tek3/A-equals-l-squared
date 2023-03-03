8f71a378f72d1494dbfe5a658c6d62673c9105dc
Bunny <tomduvalmahe44@gmail.com>
2023-03-03 11:25:28 +0100

    Improve Front-End #113
     - Change to display our logo with a given size

--- application/app_code/lib/material_lib_functions/material_functions.dart
+++ application/app_code/lib/material_lib_functions/material_functions.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 /// This function display our logo
 Widget displayLogo(double size) {
   return Column(
-    children: <Widget>[Image.asset('assets/icons/Area_Logo.png')],
+    children: <Widget>[Image.asset('assets/icons/Area_Logo.png', height: size, width: size,),],
   );
 }
 
